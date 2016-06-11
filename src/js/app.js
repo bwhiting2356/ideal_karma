156838944287f0bc04cd0e7098d1cf65d3071344
Brendan Whiting <brendan.whiting@me.com>
2016-06-11 06:04:46 -0400

    time to upload

--- src/js/app.js
+++ src/js/app.js
@@ -1,28 +1,46 @@
 var url = "mock/mock.json";
 
-var $accounts_table = $('#user-accounts');
-var $right_panel = $('#right-panel')
+var users = [];
+var transactions = [];
 
-var make_active_user = function() {
-    // remove active class from all the rows
-    $.each($accounts_table.children(), function(index, value) {
-        $(value).removeClass('active');
-    });
-    $(this).addClass('active');
-    $(this).attr('data-id');
-    $right_panel.addClass('showing');
-}
+var $user_list = $('#user-list');
+var $right_panel = $('#right-panel');
+var $transaction_list = $('#transaction-list');
+var $account_details_phone = $right_panel.find('.col-phone');
+var $account_details_date = $right_panel.find('.col-regis-date');
+var $account_details_balance = $right_panel.find('.col-balance');
+var $chart_container = $('#chart-container');
+
+// chart configurations
+
+var chart_options = {
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero:true
+            }
+        }]
+    },
+    legend: {
+        display: false
+    }
+};
+
+var chart_labels = ["Jan", "Feb", "March", "April", "May", "June"];
+    
+
+// fetch mock data
 
 $.getJSON(url, function(response) {
-    var users = response.users;
-    var transactions = response.transactions;
+    users = response.users;
+    transactions = response.transactions;
     console.log(users, transactions);
 
     $.each(users, function(index, value) {
         var user_div = $('<div></div>')
             .addClass('user-row')
             .attr("data-id", value.id)
-            .click(make_active_user);
+            .click(make_current_user);
         var phone = $('<div></div>')
             .addClass('col-phone')
             .text(value.phone);
@@ -33,9 +51,108 @@ $.getJSON(url, function(response) {
             .addClass('col-balance')
             .text(value.balance);
 
-        user_div.append(phone).append(regis_date).append(balance);
-        $accounts_table.append(user_div);
-    })
-
-
+        user_div.append(phone)
+                .append(regis_date)
+                .append(balance);
+        $user_list.append(user_div);
+    });
 });
+
+// find the user object from the array from its id
+
+function find_user(users, id) {
+    for (var i = 0; i < users.length; i++) {
+        if (parseInt(users[i].id) == id) {
+            return users[i];
+        }
+    }
+}
+
+// find all the transactions where the user is sender or recipient
+
+function find_transactions(transactions, id) {
+    var user_transactions = [];
+    for (var i = 0; i < transactions.length; i++) {
+        var sender_id = transactions[i].sender;
+        var recipient_id = transactions[i].recipient;
+        if ((sender_id === parseInt(id)) || (recipient_id === parseInt(id))) {
+            user_transactions.push(transactions[i]);
+           }
+    }
+    return user_transactions;
+}
+
+/* when someone clicks on a user in the left panel, 
+ it will fill out the right panel with their transactions */
+
+var make_current_user = function() {
+    $right_panel.show();   // need to set to show here to avoid chart bug on the first click
+
+    // remove active class from all the rows
+    $.each($user_list.children(), function(index, value) {
+        $(value).removeClass('active');
+    });
+
+    // make this row the active row
+    $(this).addClass('active');
+
+    // show that user's account details in the right panel
+    var id = $(this).attr('data-id');
+    var current_user = find_user(users, id);
+    $account_details_phone.text(current_user.phone);
+    $account_details_date.text(current_user['regis-date']);
+    $account_details_balance.text(current_user.balance); 
+
+    $transaction_list.empty();
+    var user_transactions = find_transactions(transactions, id);
+
+    // fill out transaction list
+
+    $.each(user_transactions , function(index, value) {
+        var transaction_div = $('<div></div>')
+            .addClass('transaction-row')
+            .attr("data-id", value.id);
+        var sender = $('<div></div>')
+            .addClass('col-sender')
+            .text(find_user(users, value.sender).phone);  // this is probably REALLY inefficient, sorry.......
+        var recipient = $('<div></div>')
+            .addClass('col-recipient')
+            .text(find_user(users, value.recipient).phone);  // this is probably REALLY inefficient, sorry.......
+        var datetime = $('<div></div>')
+            .addClass('col-datetime')
+            .text(value.datetime);
+        var amount = $('<div></div>')
+            .addClass('col-amount')
+            .text(value.amount);
+        transaction_div.append(sender)
+                       .append(recipient)
+                       .append(datetime)
+                       .append(amount);
+        $transaction_list.append(transaction_div);
+    });
+
+    // reset and make chart
+    
+    $chart_container.empty();
+    var $chart_canvas = $('<canvas id="myChart" width="570" height="300"></canvas>');
+    $chart_container.append($chart_canvas);
+    
+    var myChart = new Chart($chart_canvas, {
+        type: 'line',
+        data: {
+            labels: chart_labels,
+            datasets: [{
+                data: [0, 19, 3, 5, 2, 3],
+                backgroundColor: ['rgba(109,176,45,0.2)'],
+                borderColor: ['#6DB02D'],
+                borderWidth: 1
+            }]
+        },
+        options: chart_options
+    });
+
+
+    // animate in right panel again
+    $right_panel.css('top', '-1000px');
+    $right_panel.animate({"top": "0px"}, "1.3s");
+};
